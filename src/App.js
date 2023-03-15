84a08c19e7ec97b00504358f52394af1e02bbca9
Dhiraj Naik <naikdhiraj9@gmail.com>
2023-03-15 15:22:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ReactPaginate from "react-paginate";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await fetch(`https://reqres.in/api/users/`);
+      const json = await res.json();
+      setUsers(json.data);
+    };
+    getUsers();
+  }, []);
+  console.log(users);
+  const handlePageClick = (users) => {
+    fetch(`https://reqres.in/api/users/?id=${users.selected}`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json.data);
+        const markup = json.data.map((el) => {
+          return ` <div key={el.id}>
+        <div className="card align-items-center mb-3">
+          <img
+            key=${el.avatar}
+            src=${el.avatar}
+            className="card-img-top"
+            alt="user photograph"
+          />
+          <div className="card-body">
+            <h3 className="card-title">
+              ${el.first_name + el.last_name}
+            </h3>
+            <p>${el.email}</p>
+            <p>${el.id}</p>
+          </div>
+        </div>
+      </div>
+      `;
+        });
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <div className="card align-items-center mb-3">
+              <img
+                key={user.avatar}
+                src={user.avatar}
+                className="card-img-top"
+                alt="user photograph"
+              />
+              <div className="card-body">
+                <h3 className="card-title">
+                  {user.first_name + user.last_name}
+                </h3>
+                <p>{user.email}</p>
+                <p>{user.id}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+
+      <ReactPaginate
+        previousLabel="<<"
+        nextLabel=">>"
+        breakLabel="..."
+        pageCount={6}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousLinkClassName={"page-link"}
+        nextLinkClassName={"page-link"}
+        activeClassName={"active"}
+      />
     </div>
   );
 }
